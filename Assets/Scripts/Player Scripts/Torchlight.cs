d08b95d0f6bdc3895079f1455796dc92ff3d3fcd
Noah Gorgevski-Sharpe <ngsharpe13@gmail.com>
2023-03-24 20:59:42 -0400

    Finalized Alpha Build

--- Assets/Scripts/Player Scripts/Torchlight.cs
+++ Assets/Scripts/Player Scripts/Torchlight.cs
@@ -13,29 +13,32 @@ public class Torchlight : MonoBehaviour
     public float intensity = 1f;
     public float range = 5f;
 
-    private Light torchLight;
+    //private Light torchLight;
+
+    [SerializeField]
+    private GameObject player;
 
     void Start()
     {
-        torchLight = GetComponent<Light>();
-        torchLight.intensity = intensity;
-        torchLight.range = range;
+        //torchLight = GetComponent<Light>();
+        //torchLight.intensity = intensity;
+        //torchLight.range = range;
     }
 
     void Update()
     {
         // Set torch position to player position
-        transform.position = new Vector3(PlayerMovement.instance.transform.position.x, PlayerMovement.instance.transform.position.y, transform.position.z);
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z);
 
         // Update torch rotation to follow player rotation
-        transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, PlayerMovement.instance.transform.eulerAngles.z));
+        transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, player.transform.eulerAngles.z));
 
         // Update torchlight intensity based on player movement speed
-        float movementSpeed = PlayerMovement.instance.GetMovementSpeed();
-        torchLight.intensity = intensity + movementSpeed;
+        float movementSpeed = player.GetComponent<PlayerMovement>().runSpeed;
+        //torchLight.intensity = intensity + movementSpeed;
 
         // Update torchlight range based on player movement speed
-        torchLight.range = range + movementSpeed;
+        //torchLight.range = range + movementSpeed;
     }
 }
 
