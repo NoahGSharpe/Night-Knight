118184076e525650c6da8273c680d4fc03852850
babyCoderOder <86491066+babyCoderOder@users.noreply.github.com>
2023-04-06 18:14:46 -0400

    big pull request
    level 1 and stuff

---
+++ Assets/Scripts/HAZARDS.cs
@@ -0,0 +1,20 @@
+﻿/* 
+    Cristopher Argueta
+    MIDTERM
+    Filename: HAZARDS.cs
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HAZARDS : MonoBehaviour
+{
+    [SerializeField] private float damage;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //if (Collider.tag == "Player")
+           // collision.GetComponent<Health>().TakeDamage(damage);
+    }
+
+}
